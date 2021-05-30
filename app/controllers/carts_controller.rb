0e303353ff7633ccd9800804b34b3a651b4cedba
Art <artelo.studio@gmail.com>
2021-05-30 16:04:23 +0000

    carts application controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,24 +1,12 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class CartsController < ApplicationController
+  def show
+    @cart = @current_cart
+  end
 
-  before_action :current_cart
-
-  private
-    def current_cart
-      if session[:cart_id]
-        cart = Cart.find_by(:id => session[:cart_id])
-        if cart.present?
-          @current_cart = cart
-        else
-          session[:cart_id] = nil
-        end
-      end
-
-      if session[:cart_id] == nil
-        @current_cart = Cart.create
-        session[:cart_id] = @current_cart.id
-      end
-    end
+  def destroy
+    @cart = @current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+    redirect_to root_path
+  end
 end
