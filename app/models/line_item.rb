df614c324eccaa87f14350f00000043d7cc48286
Art <artelo.studio@gmail.com>
2021-05-30 16:13:44 +0000

    models helper

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
-  belongs_to :order 
-end
+  belongs_to :order
+
+  def total_price
+      self.quantity * self.product.price
+    end
+  end
