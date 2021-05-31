1ad5f4b42052c4f89e3ef49a87c6528c14acdb0c
Art <artelo.studio@gmail.com>
2021-05-31 00:47:20 +0000

    create order

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -52,4 +52,20 @@ def reduce_quantity
   @line_item.save
   redirect_to cart_path(@current_cart)
 end
+def create
+  @order = Order.new(order_params)
+  @current_cart.line_items.each do |item|
+    @order.line_items << item
+    item.cart_id = nil
+  end
+  @order.save
+  Cart.destroy(session[:cart_id])
+  session[:cart_id] = nil
+  redirect_to root_path
+end
+
+private
+  def order_params
+    params.require(:order).permit(:name, :email, :address, :pay_method)
+  end
   end
